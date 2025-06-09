b6eb31b8a458e77323108485df0b4655e7a57d06
olliekeane123 <ollie.keane1@gmail.com>
2025-06-09 13:23:02 +0100

    fix import

--- data/dev-data/users.ts
+++ data/dev-data/users.ts
@@ -1,6 +1,9 @@
-import { BaseUser } from "../../types/api/users"
+type UserData = {
+    username: string
+    password: string
+}
 
-export const users: BaseUser[] = [
+export const users: UserData[] = [
     { username: "alice", password: "pass123" },
     { username: "bob", password: "pass456" },
     { username: "mikela", password: "pass789" },
